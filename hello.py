6c51f570526701ea939a452843a532178054c9ec
lastmoonth <117797070+lastmoonth@users.noreply.github.com>
2023-03-29 10:10:49 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
-// For command line git tutorial
-// First : GitHub modification
-// Second : Local repository modification
+# For command line git tutorial
+# First : GitHub modification
+# Second : Local repository modification
 
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
